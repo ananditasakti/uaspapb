02384cf1f967bf4bd4ada51fa2c761ab7c4c4bbf
anandyta sakti alamsyah <akudita182@gmail.com>
2023-08-10 11:50:12 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// IMAGES
-var meatImage =
-    'https://i.ibb.co/5FRwHtB/unnamed.jpg';
-var foodImage =
-    'https://i.ibb.co/qk6PzDy/London-broil.jpg';
-
-var burgerImage =
-    'https://images.unsplash.com/photo-1534790566855-4cb788d389ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
-var chickenImage =
-    'https://images.unsplash.com/photo-1532550907401-a500c9a57435?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
-
 // COLORS
 var textYellow = Color(0xFFf6c24d);
 var iconYellow = Color(0xFFf4bf47);
